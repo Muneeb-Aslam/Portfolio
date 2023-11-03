c4ef35ac632812306e6d8e77660b95ceaf3a7310
Muneeb Aslam <muneeb.official.686332@hotmail.com>
2023-11-03 15:20:57 +0500

    Header

---
+++ src/components/project.jsx
@@ -0,0 +1,9 @@
+
+
+function project() {
+  return (
+    <div>project</div>
+  )
+}
+
+export default project
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -10,10 +10,10 @@ export default function Home() {
                 <p className="title">Hi! <p className="name">I am Muneeb Aslam</p></p>
                 <p className="skills">Front End Developer (React JS) 💻 - Student 👩‍🎓</p>
                 <ul className="media">
-                    <li className="media-titles"><FontAwesomeIcon icon={faGithub} style={{color: "#000000",}} /></li>
-                    <li className="media-titles"><FontAwesomeIcon icon={faLinkedin} /></li>
-                    <li className="media-titles"><FontAwesomeIcon icon={faFacebook} /></li>
-                    <li className="media-titles"><FontAwesomeIcon icon={faGoogle} /></li>
+                    <li className="media-titles"><FontAwesomeIcon icon={faGithub} style={{color: "#000000",}} size="2xl"/></li>
+                    <li className="media-titles"><FontAwesomeIcon icon={faLinkedin} style={{color: "#0072b1",}} size="2xl"/></li>
+                    <li className="media-titles"><FontAwesomeIcon icon={faFacebook} style={{color: "#4267B2",}} size="2xl"/></li>
+                    <li className="media-titles"><FontAwesomeIcon icon={faGoogle} style={{color: "#DB4437",}} size="2xl"/></li>
                 </ul>
                 <button className="resume">Resume</button>
             </main>
