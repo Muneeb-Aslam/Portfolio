6e08b24ab8d1894178146d683e7d14691fed0167
Muneeb Aslam <muneeb.official.686332@hotmail.com>
2023-11-05 23:02:08 +0500

    about

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,8 +1,22 @@
 
-function about() {
+function About() {
   return (
-    <div>about</div>
+    <section className="about">
+      <div className="title">What I Do?</div>
+      <main>
+        <div className="mern">Full Stack Development</div>
+        <ul className="description">
+          <li className="points">✨Building responsive websites using React-Redux.js | HTML/CSS | TailwindCSS</li>
+          <li className="points">✨Creating Single page rendering applications using Redux</li>
+          <li className="points">✨Creating optimized application backends in Node.js, Express.js</li>
+          <li className="points">✨Experience in usage of Rest APIs in modern programming practices</li>
+        </ul>
+        <div className="languages">Languages</div>
+        <div className="tools">Tools  & Technologies</div>
+        <div className="concepts">Concepts</div>
+      </main>
+    </section>
   )
 }
 
-export default about
+export default About
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,22 +1,26 @@
 import { faLinkedin, faGithub, faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
 import Nav from "../components/Nav";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import '../assets/header.css'
+import '../assets/home.css'
+import About from "../components/about";
 export default function Home() {
     return (
-        <header>
-            <Nav />
-            <main>
-                <p className="title">Hi! <p className="name">I am Muneeb Aslam</p></p>
-                <p className="skills">Front End Developer (React JS) 💻 - Student 👩‍🎓</p>
-                <ul className="media">
-                    <li className="media-titles"><FontAwesomeIcon icon={faGithub} style={{color: "#000000",}} size="2xl"/></li>
-                    <li className="media-titles"><FontAwesomeIcon icon={faLinkedin} style={{color: "#0072b1",}} size="2xl"/></li>
-                    <li className="media-titles"><FontAwesomeIcon icon={faFacebook} style={{color: "#4267B2",}} size="2xl"/></li>
-                    <li className="media-titles"><FontAwesomeIcon icon={faGoogle} style={{color: "#DB4437",}} size="2xl"/></li>
-                </ul>
-                <button className="resume">Resume</button>
-            </main>
-        </header>
+        <>
+            <header>
+                <Nav />
+                <main>
+                    <p className="title">Hi! <p className="name">I am Muneeb Aslam</p></p>
+                    <p className="skills">Full Stack Developer (MERN) 💻 - Student 👩‍🎓</p>
+                    <ul className="media">
+                        <li className="media-titles"><FontAwesomeIcon icon={faGithub} style={{ color: "#000000", }} size="2xl" /></li>
+                        <li className="media-titles"><FontAwesomeIcon icon={faLinkedin} style={{ color: "#0072b1", }} size="2xl" /></li>
+                        <li className="media-titles"><FontAwesomeIcon icon={faFacebook} style={{ color: "#4267B2", }} size="2xl" /></li>
+                        <li className="media-titles"><FontAwesomeIcon icon={faGoogle} style={{ color: "#DB4437", }} size="2xl" /></li>
+                    </ul>
+                    <button className="resume">Resume</button>
+                </main>
+            </header>
+            <About />
+        </>
     )
 }
